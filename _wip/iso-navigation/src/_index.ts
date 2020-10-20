83e1c2445eec6dccb592accf48d938086e43ce6e
Alexis Bouhet <alex@zouloux.com>
2020-10-20 18:17:51 +0200

    Cleaned

--- _wip/iso-navigation/src/_index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./Router"
-export * from "./ViewStack"
-export * from "./Apps"
--- _wip/web-yadl/src/_index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./element";
-export * from "./event";
-export * from "./find";
-export * from "./load";
-//export * from "./size";
-export * from "./tree";
-export * from "./utils";
