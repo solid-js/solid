4ac425806a6bb1adb3e04dfd81d7ec7896dffa23
Alexis Bouhet <alex@zouloux.com>
2021-03-31 16:11:55 +0200

    node-files - 1.0.3 : Patched dot env parser

--- libraries/node-files/src/FileParsers.ts
+++ libraries/node-files/src/FileParsers.ts
@@ -14,9 +14,37 @@ export const DotEnvParser = {
 	decode ( buffer:string ) {
 		const data:ScalarObject = {};
 		buffer.split("\n").map( line => {
-			const parts = line.split("=", 2);
+			// Remove comments
+			if ( line.trim().indexOf('#') === 0 )
+				return;
+
+			// TODO : Remove comments in values only :
+			// TODO : KEY=value # This is a comment
+
+			// Split key and value
+			const parts = line.split("=");
+
+			// Continue only if their is an assignment
 			if ( parts.length < 2 ) return;
-			data[ parts[0].trim() ] = parts[1].trim();
+
+			// Extract key and value, we join to allow "=" on values
+			let key = parts.shift().trim()
+			let value = parts.join('=').trim()
+
+			// Remove quotes on value
+			if (
+				// "value"
+				(value.indexOf('"') === 0 && value.lastIndexOf('"') === value.length - 1 )
+				// 'value'
+				|| (value.indexOf("'") === 0 && value.lastIndexOf("'") === value.length - 1 )
+			) {
+				value = value.substr( 1, value.length - 2 )
+					.replace("\'", "'")
+					.replace('\"', '"')
+			}
+
+			// Register data
+			data[ key ] = value;
 		})
 		return data;
 	},
