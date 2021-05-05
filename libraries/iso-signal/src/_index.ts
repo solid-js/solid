897bee99f195d1339416f91259b5d5c967419a61
Willy Brauner <mail@willybrauner.com>
2021-05-05 09:05:10 +0200

    Mix Signal and StateSignal proposal + switch ...rest param by simple param

--- libraries/iso-signal/src/_index.ts
+++ libraries/iso-signal/src/_index.ts
@@ -2,55 +2,39 @@
 // ----------------------------------------------------------------------------- STRUCT
 
 // Type of a micro signal handler
-export type TSignalHandler <G extends any[], E = void|any> = (...rest:G) => E
+export type TSignalHandler <G = void|any, E = void|any> = (param:G) => E
 
 // Default interface of a micro signal
-export interface ISignal <G extends any[] = any[], E = void|any>
+export interface ISignal <G = void|any, E = void|any>
 {
 	on: ( handler:TSignalHandler<G, E> ) => () => void
 	off: ( handler:TSignalHandler<G, E> ) => void
-	dispatch: ( ...rest:G ) => E[]
+	dispatch: ( param:G ) => E[]
 	clear: () => void
 	readonly listeners : TSignalHandler<G, E>[]
+	readonly state: G;
 }
 
-// Extended interface of a state signal
-export interface IStateSignal <G extends any = any, E = void|any> extends ISignal<[G], E>
-{
-	dispatch: ( state:G ) => E[]
-	readonly state : G
-}
+// ----------------------------------------------------------------------------- MIX SIGNAL & STATE SIGNAL
 
-// ----------------------------------------------------------------------------- CLASSIC SIGNAL
-
-export function Signal <G extends any[] = any, E = void|any> ():ISignal<G, E> {
-	let _listeners = []
-	const off = ( handler ) => _listeners.filter( s => s != handler )
+export function Signal<G = void | any, E = void | any>(
+	_state: G = null,
+): ISignal<G, E> {
+	let _listeners = [];
+	const off = (handler) => _listeners.filter((s) => s != handler);
 	return {
-		on ( handler:TSignalHandler<G> ) {
-			_listeners.push( handler )
-			return () => off( handler )
+		on(handler: TSignalHandler<G>) {
+			_listeners.push(handler);
+			return () => off(handler);
 		},
 		off,
-		dispatch: ( ...rest:G ) => _listeners.map( h => h(...rest) ),
-		clear () { _listeners = [] },
-		get listeners () { return _listeners }
-	}
-}
-
-// ----------------------------------------------------------------------------- STATE SIGNAL
-
-export function StateSignal
-	<G extends any, E = void|any>
-	( _state:G = null, _signal = Signal<[G], E>() )
-	:IStateSignal<G, E>
-{
-	return {
-		..._signal,
-		dispatch ( state:G ) {
-			_state = state;
-			return _signal.dispatch( state )
+		dispatch: (param: G) => {
+			_state = param;
+			return _listeners.map((h) => h(param))
 		},
-		get state ():G { return _state }
-	}
+		clear() { _listeners = []  },
+		get listeners() { return _listeners },
+		get state(): G { return _state; }
+	};
 }
+
